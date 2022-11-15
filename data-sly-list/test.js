3b621d5e13909c848458e3bec8511f416b54d011
Shubham Raut <shubham162001@gmail.com>
2022-11-15 16:43:12 +0530

    Data-sly-list

---
+++ data-sly-list/test.js
@@ -0,0 +1,21 @@
+// Object
+use(function () {
+  return {
+    arr: [
+      {
+        img: "bonfire.png",
+      },
+      {
+        img: "meal.png",
+      },
+      {
+        img: "img_fire.png",
+      },
+    ],
+  };
+});
+
+// Array
+use(function () {
+  return [10, 20, 30, 40];
+});
